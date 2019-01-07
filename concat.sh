5bd2c405a59c74103b7992bbf8b1e9a060f17fe0
Stefan Knorr <sknorr@suse.de>
2019-01-07 19:37:55 +0100

    script: add sort -u etc.

--- concat.sh
+++ concat.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 {
-  wget -O- https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-  echo -e "\n\n# Addendum"
+  wget -O- https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | \
+    sed -r \
+      -e 's/[ \t]*#.*//' \
+      -e 's/[ \t]*$//' \
+      -e 's/^[ \t]*//' \
+      -e 's/[ \t]*$//' \
+      -e 's/[ \t][ \t]+/ /' \
+      | sed -n '/^$/ !p' | sort -u
+  echo -e "\n"
   sed -r 's/^/0.0.0.0 /' addendum
 } > hosts
